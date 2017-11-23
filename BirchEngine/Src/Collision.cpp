b591be191639856829fe5cd92b277d1dd11de0e9
cm.birch <cm.birch@me.com>
2017-11-23 13:30:20 +0000

    Map Parser

--- BirchEngine/Src/Collision.cpp
+++ BirchEngine/Src/Collision.cpp
@@ -20,7 +20,7 @@ bool Collision::AABB(const ColliderComponent& colA, const ColliderComponent& col
 {
 	if (AABB(colA.collider, colB.collider))
 	{
-		std::cout << colA.tag << " hit: " << colB.tag << std::endl;
+		//std::cout << colA.tag << " hit: " << colB.tag << std::endl;
 		return true;
 	}
 	else
--- BirchEngine/Src/TextureManager.cpp
+++ BirchEngine/Src/TextureManager.cpp
@@ -9,7 +9,7 @@ SDL_Texture* TextureManager::LoadTexture(const char* texture)
 	return tex;
 }
 
-void TextureManager::Draw(SDL_Texture * tex, SDL_Rect src, SDL_Rect dest)
+void TextureManager::Draw(SDL_Texture * tex, SDL_Rect src, SDL_Rect dest, SDL_RendererFlip flip)
 {
-	SDL_RenderCopy(Game::renderer, tex, &src, &dest);
+	SDL_RenderCopyEx(Game::renderer, tex, &src, &dest, NULL, NULL, flip);
 }
