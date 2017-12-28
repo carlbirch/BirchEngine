f6740040c595cf451e59b912f49595027c72296d
Steve Robinson <ssrobins@gmail.com>
2017-12-28 14:01:07 -0800

    Add CMake code to build BirchEngine and its third-party dependencies

---
+++ copy_files_to_binary_dir.cmake
@@ -0,0 +1,9 @@
+# Stage images, fonts, etc... so that builds run from the binary dir will work
+add_custom_command(
+    TARGET ${target_name}
+    POST_BUILD
+    COMMAND cmake -E copy_directory ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR}
+)
+
+install(TARGETS ${target_name} DESTINATION ${target_name})
+install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/ DESTINATION ${target_name})
