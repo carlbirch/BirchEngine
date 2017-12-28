35c915f333d89809afb0ceff3693132a16d76edd
Steve Robinson <ssrobins@gmail.com>
2017-12-28 14:25:35 -0800

    Fix paths for copying assets during the build and for the install target

--- copy_files_to_binary_dir.cmake
+++ copy_files_to_binary_dir.cmake
@@ -2,8 +2,8 @@
 add_custom_command(
     TARGET ${target_name}
     POST_BUILD
-    COMMAND cmake -E copy_directory ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR}
+    COMMAND cmake -E copy_directory ${CMAKE_CURRENT_SOURCE_DIR}/../assets ${CMAKE_CURRENT_BINARY_DIR}/assets
 )
 
 install(TARGETS ${target_name} DESTINATION ${target_name})
-install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/ DESTINATION ${target_name})
+install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/../assets DESTINATION ${target_name})
