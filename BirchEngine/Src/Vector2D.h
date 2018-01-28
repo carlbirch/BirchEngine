fda4c9737656ebbca59221c5b13c7a50385c1474
Tedi <hkanelhs@yahoo.gr>
2018-01-28 12:08:17 +0200

    BUG Fix: Vector2D operators

--- BirchEngine/Src/Vector2D.h
+++ BirchEngine/Src/Vector2D.h
@@ -15,10 +15,10 @@ public:
 	Vector2D& Multiply(const Vector2D& vec);
 	Vector2D& Divide(const Vector2D& vec);
 
-	friend Vector2D& operator+(Vector2D& v1, const Vector2D& v2);
-	friend Vector2D& operator-(Vector2D& v1, const Vector2D& v2);
-	friend Vector2D& operator*(Vector2D& v1, const Vector2D& v2);
-	friend Vector2D& operator/(Vector2D& v1, const Vector2D& v2);
+	Vector2D operator+ ( const Vector2D& vec );
+	Vector2D operator- ( const Vector2D& vec );
+	Vector2D operator* ( const Vector2D& vec );
+	Vector2D operator/ ( const Vector2D& vec );
 
 	Vector2D& operator+=(const Vector2D& vec);
 	Vector2D& operator-=(const Vector2D& vec);
